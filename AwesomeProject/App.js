4e8c230ffc974bc3a4f3837b62f4e9699d28b174
VAN HUYNH VIEN DONG <dongvhv@caters.co.jp>
2019-04-03 17:01:17 +0900

    Add first app

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,49 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+import React from 'react';
+import { StyleSheet, Text, Platform, ImageBackground, View, KeyboardAvoidingView, ActivityIndicator, StatusBar } from 'react-native';
+import SearchInput from './components/SearchInput';
+import getImageForWeather from './utils/getImageForWeather';
+import { fetchLocationId, fetchWeather } from './utils/api';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      error: false,
+      location: '',
+      temperature: 0,
+      weather: ''
+    };
+  }
+  handleUpdateLocation = async (city) => {
+    if (!city) return;
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(locationId);
+        this.setState({ loading: false, error: false, location, weather, temperature });
+      } catch (e) {
+        this.setState({ loading: false, error: true });
+      }
+    });
+  };
+  componentDidMount() {
+    this.handleUpdateLocation('London');
+  }
   render() {
+    const { loading, error, weather, temperature, location } = this.state;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
+        <ImageBackground source={getImageForWeather(weather)} style={styles.imageContainer} imageStyle={styles.image}>
+          <View style={styles.detailsContainer}>
+            <ActivityIndicator animating={loading} color="white" size="large" />
+            {!loading && (
+              <View>
+                {error && <Text style={[styles.smallText, styles.textStyle]}>Could not load weather, please try a different city.</Text>}
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>{weather}</Text>
+                    <Text style={[styles.largeText, styles.textStyle]}>{`${Math.round(temperature)}°`} </Text>
+                  </View>
+                )}
+                <SearchInput placeholder="Search any city" onSubmit={this.handleUpdateLocation} />
+              </View>
+            )}
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#34495E',
+  },
+  detailsContainer: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20
   },
-  welcome: {
-    fontSize: 20,
+  textStyle: {
     textAlign: 'center',
-    margin: 10,
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  largeText: {
+    fontSize: 44
   },
+  smallText: {
+    fontSize: 18
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center'
+  },
+  imageContainer: {
+    flex: 1
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover'
+  }
 });
